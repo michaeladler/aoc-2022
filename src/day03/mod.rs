5d7cbc83830ca31a0526c809376e800a37d87a28
Michael Adler <therisen06@gmail.com>
2022-12-06 07:59:45 +0100

    Day 3: Optimize using bitsets

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,18 +1,14 @@
-use std::collections::HashSet;
-
 use arrayvec::ArrayVec;
 use log::debug;
 
+use aoc_lib::bitset::Bitset;
 use aoc_lib::parse;
 
 pub fn solve(input: &[u8]) -> (String, String) {
-    let mut input = input;
-
     let mut part1: i32 = 0;
     let mut part2: i32 = 0;
-
-    let mut elves: ArrayVec<HashSet<u8>, 3> = ArrayVec::new();
-
+    let mut elves: ArrayVec<Bitset, 3> = ArrayVec::new();
+    let mut input = input;
     while !input.is_empty() {
         let mut n: usize = 0;
         for &b in input.iter() {
@@ -25,34 +21,35 @@ pub fn solve(input: &[u8]) -> (String, String) {
         let lhs = &input[0..mid];
         let rhs = &input[mid..n];
 
-        // TODO: use bitsets to represent rucksack
-        //let lhs_rucksack: u64 = 0;
-        //let rhs_rucksack: u64 = 0;
-        let mut items = HashSet::with_capacity(n);
-        let mut lhs_rucksack = HashSet::with_capacity(mid);
+        let mut lhs_rucksack = Bitset::new();
+        let mut rhs_rucksack = Bitset::new();
         for &b in lhs.iter() {
-            lhs_rucksack.insert(b);
-            items.insert(b);
+            lhs_rucksack.set((b - b'A') as usize);
         }
-        let mut rhs_rucksack = HashSet::with_capacity(mid);
         for &b in rhs.iter() {
-            rhs_rucksack.insert(b);
-            items.insert(b);
+            rhs_rucksack.set((b - b'A') as usize);
         }
+        let items = lhs_rucksack.union(rhs_rucksack);
 
-        for &b in lhs_rucksack.intersection(&rhs_rucksack) {
+        let intersection = lhs_rucksack.intersect(rhs_rucksack);
+        for k in intersection.iter() {
+            let b: u8 = b'A' + k as u8;
             let prio = calc_prio(b);
             debug!("common: {}, prio: {prio}", b as char);
             part1 += prio as i32;
         }
-        // TODO: unchecked
-        elves.push(items);
+        unsafe {
+            elves.push_unchecked(items);
+        }
         if elves.len() == 3 {
-            // from https://github.com/rust-lang/rfcs/issues/2023
-            let intersection = elves.iter().skip(1).fold(elves[0].clone(), |acc, hs| {
-                acc.intersection(hs).cloned().collect()
-            });
-            for &b in intersection.iter() {
+            let intersection: Bitset = unsafe {
+                elves
+                    .get_unchecked(0)
+                    .intersect(*elves.get_unchecked(1))
+                    .intersect(*elves.get_unchecked(2))
+            };
+            for k in intersection.iter() {
+                let b: u8 = b'A' + k as u8;
                 let prio = calc_prio(b);
                 part2 += prio as i32;
             }
