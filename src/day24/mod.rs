1e346e6e1271913967e29b6801d8629822fffa22
Michael Adler <adlerm@mailbox.org>
2023-01-20 20:36:27 +0100

    Day 24, Part 1: Optimize
    Blizzards repeat! Use lcm of width and height to find cycle_len.

--- src/day24/mod.rs
+++ src/day24/mod.rs
@@ -1,15 +1,20 @@
+mod blizzard;
 mod state;
 
 use ahash::AHashMap;
 use aoc_lib::{parse, point::Point2D};
 use binary_heap_plus::{BinaryHeap, MinComparator};
-use log::debug;
+use log::{debug, trace};
+use num_integer::Integer;
 
 use state::Node;
 
-pub fn solve(input: &[u8]) -> (u32, u32) {
-    let mut initial: Node = Node::new();
+use crate::day24::blizzard::Blizzard;
 
+pub fn solve(input: &[u8]) -> (u32, u32) {
+    let mut blizz = Blizzard::new();
+
+    let start: Point2D<i32>;
     let end: Point2D<i32>;
     {
         let mut input = input;
@@ -26,7 +31,7 @@ pub fn solve(input: &[u8]) -> (u32, u32) {
             for (x, b) in input.iter().enumerate() {
                 match b {
                     b'>' | b'<' | b'v' | b'^' => {
-                        initial.add_blizz(x, y, *b);
+                        blizz.add_blizz(x, y, *b);
                     }
                     b'\n' => {
                         input = &input[x..];
@@ -37,65 +42,79 @@ pub fn solve(input: &[u8]) -> (u32, u32) {
             }
             input = parse::seek_next_line(input);
         }
-        initial.loc = Point2D::new(x_start as i32, 0);
+        start = Point2D::new(x_start as i32, 0);
         end = Point2D::new(x_end as i32, y as i32);
     }
 
-    let part1 = a_star(initial, end);
+    debug!("start: {:?}, end: {:?}", start, end);
+    let height = end.y - 1;
+    let width = end.x - 1;
+    let cycle_len = height.lcm(&width) as usize;
+    debug!("cycle_len: lcm({width}, {height})={cycle_len}");
+
+    let mut blizz_configs = Vec::with_capacity(cycle_len);
+    for _ in 0..cycle_len {
+        blizz_configs.push(blizz.clone());
+        blizz = blizz.move_blizzards(end);
+    }
+
+    let part1 = a_star(start, end, &blizz_configs);
     let part2 = 42;
     (part1, part2)
 }
 
 // Based on https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode
-fn a_star(initial: Node, end: Point2D<i32>) -> u32 {
+fn a_star(start: Point2D<i32>, end: Point2D<i32>, blizz_configs: &[Blizzard]) -> u32 {
     const INFINITY: u32 = u32::MAX - 10;
 
-    let start = initial.loc;
     let h = |current: Point2D<i32>| end.manhattan(current) as u32;
+
+    let initial = Node::new(start);
 
     // For node n, gScore[n] is the cost of the cheapest path from start to n currently known.
     let mut g_score: AHashMap<Node, u32> = AHashMap::with_capacity(1024);
-    g_score.insert(initial.clone(), 0);
+    g_score.insert(initial, 0);
 
     // The set of discovered nodes that may need to be (re-)expanded.
     let mut open: BinaryHeap<(u32, Node), MinComparator> = BinaryHeap::with_capacity_min(1024);
     open.push((0, initial));
 
-    // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
-    // to n currently known.
-    let mut came_from: AHashMap<Node, Node> = AHashMap::with_capacity(1024);
+    // For node n, cameFrom[n] is the node immediately preceding it on the
+    // cheapest path from start to n currently known.
+    //let mut came_from: AHashMap<Node, Node> = AHashMap::with_capacity(512);
 
     let mut neighbors = Vec::with_capacity(5);
     while let Some((_, current)) = open.pop() {
-        if current.loc == end {
-            let dist = *g_score.get(&current).unwrap();
-            return dist;
+        if current.position == end {
+            return *g_score.get(&current).unwrap();
         }
 
-        let tmp = current.move_blizzards(end);
         let g_current = g_score.get(&current).copied().unwrap_or(INFINITY);
         // time advances by 1
+        let minute = (current.blizz_idx + 1) % blizz_configs.len() as u32;
         let tentative_g_score = g_current + 1;
-        debug!("tentative_g_score: {tentative_g_score}");
+        trace!("tentative_g_score: {tentative_g_score}");
 
         neighbors.clear();
-        tmp.neighbors(start, end, &mut neighbors);
-        neighbors.push(current.loc); // not moving is always an option and leads to a new node in the graph
-                                     // however, if a blizzard wants to take this position, we
-                                     // *must* move!
+        current.neighbors(start, end, &mut neighbors);
+        // not moving is an option and leads to a new node in the graph;
+        // however, if a blizzard wants to take this position, we must move!
+        neighbors.push(current.position);
+
+        let blizz = blizz_configs.get(minute as usize).unwrap();
 
         for nb in neighbors
             .iter()
-            .filter(|&&candidate| !tmp.is_blizzard(candidate))
+            .filter(|&&candidate| !blizz.is_blizzard(candidate))
         {
             // tentative_gScore is the distance from start to the neighbor through current
-            let mut neighbor_state = tmp.clone();
-            neighbor_state.loc = *nb;
+            let mut neighbor_state = current;
+            neighbor_state.blizz_idx = minute;
+            neighbor_state.position = *nb;
             if tentative_g_score < g_score.get(&neighbor_state).copied().unwrap_or(INFINITY) {
-                debug!("This path to neighbor is better than any previous one. Record it!");
-                // TODO: not needed here
-                came_from.insert(neighbor_state.clone(), current.clone());
-                g_score.insert(neighbor_state.clone(), tentative_g_score);
+                trace!("This path to neighbor is better than any previous one. Record it!");
+                //came_from.insert(neighbor_state, current);
+                g_score.insert(neighbor_state, tentative_g_score);
                 let f_score_nb = tentative_g_score + h(*nb);
                 if !open.iter().any(|other| other.1 == neighbor_state) {
                     open.push((f_score_nb, neighbor_state));
--- src/day24/state.rs
+++ src/day24/state.rs
@@ -1,71 +1,17 @@
 use aoc_lib::point::Point2D;
 
-const MAX_WIDTH: usize = 122;
-const MAX_HEIGHT: usize = 27;
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Node {
-    pub loc: Point2D<i32>,
-    // indexed by row
-    right: [u128; MAX_HEIGHT],
-    // indexed by row
-    left: [u128; MAX_HEIGHT],
-    // indexed by column
-    up: [u32; MAX_WIDTH],
-    // indexed by column
-    down: [u32; MAX_WIDTH],
+    pub position: Point2D<i32>,
+    pub blizz_idx: u32,
 }
 
 impl Node {
-    pub fn new() -> Self {
+    pub fn new(start: Point2D<i32>) -> Self {
         Self {
-            loc: Point2D::new(0, 0),
-            right: [0; MAX_HEIGHT],
-            left: [0; MAX_HEIGHT],
-            up: [0; MAX_WIDTH],
-            down: [0; MAX_WIDTH],
+            position: start,
+            blizz_idx: 0,
         }
-    }
-
-    pub fn add_blizz(&mut self, x: usize, y: usize, b: u8) {
-        match b {
-            b'>' => {
-                self.right[y] |= 1 << x;
-            }
-            b'<' => {
-                self.left[y] |= 1 << x;
-            }
-            b'v' => {
-                self.down[x] |= 1 << y;
-            }
-            b'^' => {
-                self.up[x] |= 1 << y;
-            }
-            _ => {}
-        }
-    }
-
-    pub fn move_blizzards(&self, end: Point2D<i32>) -> Self {
-        let mut result = self.clone();
-        let width = end.x;
-        for row in result.left.iter_mut() {
-            let x = *row;
-            *row = x >> 1 | x << (width - 1);
-        }
-        for row in result.right.iter_mut() {
-            let x = *row;
-            *row = x << 1 | x >> (width - 1);
-        }
-        let height = end.y - 1;
-        for col in result.up.iter_mut() {
-            let x = *col;
-            *col = x >> 1 | x << (height - 1);
-        }
-        for col in result.down.iter_mut() {
-            let x = *col;
-            *col = x << 1 | x >> (height - 1);
-        }
-        result
     }
 
     pub fn neighbors(&self, start: Point2D<i32>, end: Point2D<i32>, out: &mut Vec<Point2D<i32>>) {
@@ -76,7 +22,7 @@ impl Node {
             Point2D::new(-1, 0),
         ];
         for d in deltas {
-            let candidate = self.loc + d;
+            let candidate = self.position + d;
             if (candidate == start || candidate == end)
                 || (candidate.x > 0
                     && candidate.x <= end.x
@@ -87,75 +33,4 @@ impl Node {
             }
         }
     }
-
-    pub fn is_blizzard(&self, p: Point2D<i32>) -> bool {
-        self.left_blizz(p) || self.right_blizz(p) || self.up_blizz(p) || self.down_blizz(p)
-    }
-
-    fn left_blizz(&self, p: Point2D<i32>) -> bool {
-        (self.left[p.y as usize] & (1 << p.x as usize)) != 0
-    }
-
-    fn right_blizz(&self, p: Point2D<i32>) -> bool {
-        (self.right[p.y as usize] & (1 << p.x as usize)) != 0
-    }
-
-    fn up_blizz(&self, p: Point2D<i32>) -> bool {
-        (self.up[p.x as usize] & (1 << p.y as usize)) != 0
-    }
-
-    fn down_blizz(&self, p: Point2D<i32>) -> bool {
-        (self.down[p.x as usize] & (1 << p.y as usize)) != 0
-    }
-
-    /*
-    pub fn print(&self, start: Point2D<i32>, end: Point2D<i32>) {
-        println!("State:");
-        let width = end.x as usize + 1;
-        for i in 0..=width {
-            if i as i32 == start.x {
-                print!(".");
-            } else {
-                print!("#");
-            }
-        }
-        println!();
-
-        for y in 1..end.y as usize {
-            print!("#");
-            for x in 1..width {
-                let p = Point2D::new(x as i32, y as i32);
-                let count = (self.left_blizz(p) as u32)
-                    + (self.right_blizz(p) as u32)
-                    + (self.up_blizz(p) as u32)
-                    + (self.down_blizz(p) as u32);
-                if count > 1 {
-                    print!("{count}");
-                } else if self.left_blizz(p) {
-                    print!("<");
-                } else if self.right_blizz(p) {
-                    print!(">");
-                } else if self.up_blizz(p) {
-                    print!("^");
-                } else if self.down_blizz(p) {
-                    print!("v");
-                } else if self.loc == Point2D::new(x as i32, y as i32) {
-                    print!("E");
-                } else {
-                    print!(".");
-                }
-            }
-            println!("#");
-        }
-
-        for i in 0..=width {
-            if i as i32 == end.x {
-                print!(".");
-            } else {
-                print!("#");
-            }
-        }
-        println!();
-    }
-    */
 }
